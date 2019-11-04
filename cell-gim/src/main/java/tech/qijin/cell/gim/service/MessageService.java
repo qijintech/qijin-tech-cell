fd2ab4090a8a6ee7f7fdefb8651b6eb67b5a4a3a
yangshangqiang <yangshangqiang@inke.cn>
2019-11-04 21:16:05 +0800

    1. init cell-im 2. init cell-gim

---
+++ cell-gim/src/main/java/tech/qijin/cell/gim/service/MessageService.java
@@ -0,0 +1,9 @@
+package tech.qijin.cell.gim.service;
+
+/**
+ * @author michealyang
+ * @date 2019-11-04
+ * @relax: 开始眼保健操 ←_← ↓_↓ →_→ ↑_↑
+ */
+public interface MessageService {
+}
---
+++ cell-gim/src/test/java/tech/qijin/cell/gim/test/CellDemoTest.java
@@ -0,0 +1,18 @@
+package tech.qijin.cell.gim.test;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author UnitTest
+ */
+
+@SpringBootApplication(scanBasePackages = {"tech.qijin.cell.im"})
+@MapperScan("tech.qijin.cell.im.db.dao")
+public class CellDemoTest {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CellDemoTest.class, args);
+    }
+}
